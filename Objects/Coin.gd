9d9e898dbd9885f46a55b918ec6725f6a9979399
JohannesMHL <mahl.j@icloud.com>
2020-05-13 12:04:17 +0200

    coin
    describtion to coin added

--- Objects/Coin.gd
+++ Objects/Coin.gd
@@ -1,13 +1,15 @@
 """
-@auth Johannes Mahl
+Coin.gd
+@auth JM
+
 """
 extends Area2D
 
 
-onready var anim_player: AnimationPlayer = get_node("AnimationPlayer")
+onready var anim_player: AnimationPlayer = get_node("AnimationPlayer") # reference to the animation
 
 
 
 
-func _on_body_entered(body: Node) -> void:
-	anim_player.play("fade_out")
+func _on_body_entered(body: Node) -> void: # if the player collides with the coin will disapear 
+	anim_player.play("fade_out") #  the associated animation is played

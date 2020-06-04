79b3df6ed6bcc7f8a39d6522c0ddd8d37d9d799c
BlackVirusEXE <david.schmid@schmid-portal.de>
2020-06-05 00:56:10 +0200

    Adding Skin Diplaying in Selection

--- ui/SkinSelection.gd
+++ ui/SkinSelection.gd
@@ -17,14 +17,29 @@ func _ready():
 	add_item("Hans", 0)
 	add_item("Kara", 1)
 	
+	$Female1.visible = false
+	$Male1.visible = false
+	
 	writing_skin_in_config(skin)
 
 func _process(delta):
 	skin = get_selected_id()
 	writing_skin_in_config(skin)
+	displaying_Skin(skin)
+	
 
 func writing_skin_in_config(_skin_cfg: int):
 	config.load("config.cfg")
 	config.set_value("player","skin",_skin_cfg)
 	config.save("config.cfg")
+	
+func displaying_Skin(_skin_to_display: int):
+	$Female1.visible = false
+	$Male1.visible = false
+	if _skin_to_display == 0:
+		$Male1.visible = true
+		$Female1.visible = false
+	if _skin_to_display == 1:
+		$Male1.visible = false
+		$Female1.visible = true
 

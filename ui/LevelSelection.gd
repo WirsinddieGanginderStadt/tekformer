e9cbaccff7629a3db6ce21a721baa9dee13c0a65
JohannesMHL <mahl.j@icloud.com>
2020-06-05 16:28:37 +0200

    levellock

--- ui/LevelSelection.gd
+++ ui/LevelSelection.gd
@@ -6,7 +6,31 @@
 
 extends TextureButton
 
+var path = "res://data/levellock.json"
+var data 
 
+func _ready() -> void:
+	load_game()
+	button_disabled()
+
+func load_game():
+	var file = File.new()
+	file.open(path, file.READ)
+	var text = file.get_as_text()
+	data = parse_json(text)
+	file.close()
+
+
+func save_game():
+	var file
+	file = File.new()
+	file.open(path, File.WRITE)
+	file.store_line(to_json(data))
+	file.close()
+
+func button_disabled():
+	if data["world1"]["1-2"] == false:
+		$Buttons/TextureButton.visible = true
 
 """ _ON_TEXTUREBUTTON_BUTTON_DOWN: detects a click on the button """
 

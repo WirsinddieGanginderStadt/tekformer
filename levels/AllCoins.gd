a05d829d4b93c846156e39221426390f4be3ce2d
BlackVirusEXE <david.schmid@schmid-portal.de>
2020-06-06 14:01:37 +0200

    Reseting Coins implemented

--- levels/AllCoins.gd
+++ levels/AllCoins.gd
@@ -8,9 +8,11 @@ extends Node
 
 var coins_collected := 0
 var child_count := 0
+var all_childs := []
 
 func _ready():
 	child_count = get_child_count() 
+	#all_childs = get_children()
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -20,8 +22,7 @@ func _process(delta):
 			coins_collected = coins_collected +1
 		get_child(i)._is_coin_collected = false # stoping coins from getting collected multipel times
 
-#WIP
+#resets all Coins that are Children of AllCoins
 func reseting_all_Coins():
-	for i in child_count: #goes throw every child Note equal to all level Coins
-		#get_child(i).reseting_Coin()
-		pass
+	for i in get_children(): #goes throw every child Note equal to all level Coins
+		i.reseting_Coin()

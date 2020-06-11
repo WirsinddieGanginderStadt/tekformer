fc24b0a8b2cfbdf526b2634cfc331dfae5c303f8
BlackVirusEXE <david.schmid@schmid-portal.de>
2020-06-11 11:16:47 +0200

    Coin-Animations BugFix

--- Objects/Coin.gd
+++ Objects/Coin.gd
@@ -7,10 +7,15 @@ extends Area2D
 
 
 onready var anim_player: AnimationPlayer = get_node("AnimationPlayer") # reference to the animation
-
+func _ready():
+	anim_player.play("bouncing")
+	
 
 var _is_coin_collected := false  #var for counting the collected coins: false if not collected yet
 
+#func _process(delta) -> void:
+#	if _is_coin_collected == false :
+#		anim_player.play("bouncing")
 
 func _on_body_entered(body: Node) -> void: # run if the player collides with the coin
 	_is_coin_collected = true   #collects the coin
@@ -20,6 +25,8 @@ func _on_body_entered(body: Node) -> void: # run if the player collides with the
 
 #resets the Coin 
 func reseting_Coin():
+	anim_player.play_backwards("fade_out",0.5)
 	_is_coin_collected = false
 	print("[Coin]: ",name ,":  reset")
-	anim_player.play_backwards("fade_out", 0)
+	anim_player.queue("bouncing")
+	#anim_player.play("bouncing")
